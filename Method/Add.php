6538e4585206f4ee4a0ef8ef4799d14f9a0825ba
gizmore <gizmore@wechall.net>
2022-08-09 07:49:20 +0200

    '7.0.1-r1411'

--- Method/Add.php
+++ Method/Add.php
@@ -4,41 +4,45 @@ namespace GDO\Quotes\Method;
 use GDO\Form\GDT_Form;
 use GDO\Form\MethodForm;
 use GDO\Core\GDO;
-use GDO\Core\Website;
 use GDO\Quotes\GDO_Quote;
 use GDO\Form\GDT_AntiCSRF;
 use GDO\Form\GDT_Submit;
 
 final class Add extends MethodForm
 {
-    public function gdoTable() : GDO
-    {
-        return GDO_Quote::table();
-    }
-    
-    public function getTitleLangKey() { return 'link_add_quote'; }
-    
-    public function createForm(GDT_Form $form) : void
-    {
-        $table = $this->gdoTable();
-        $form->addFields([
-            $table->gdoColumn('quote_text'),
-            $table->gdoColumn('quote_date'),
-            $table->gdoColumn('quote_from'),
-            $table->gdoColumn('quote_by'),
-            GDT_AntiCSRF::make(),
-        ]);
-        $form->actions()->addField(GDT_Submit::make()->icon('plus')->label('btn_create'));
-    }
 
-    public function formValidated(GDT_Form $form)
-    {
-        $quote = GDO_Quote::blank($form->getFormVars())->insert();
-        
-        $this->redirectMessage(
-            'msg_quote_added', [$quote->getID()],
-            href('Quotes', 'Table'));
-    }
-    
+	public function gdoTable(): GDO
+	{
+		return GDO_Quote::table();
+	}
+
+	public function getMethodTitle() : string
+	{
+		return t('link_add_quote');
+	}
+
+	public function createForm(GDT_Form $form): void
+	{
+		$table = $this->gdoTable();
+		$form->addFields(
+			$table->gdoColumn('quote_text'),
+			$table->gdoColumn('quote_date'),
+			$table->gdoColumn('quote_from'),
+			$table->gdoColumn('quote_by'),
+			GDT_AntiCSRF::make(),
+		);
+		$form->actions()->addField(GDT_Submit::make()->icon('plus')
+			->label('btn_create'));
+	}
+
+	public function formValidated(GDT_Form $form)
+	{
+		$quote = GDO_Quote::blank($form->getFormVars())->insert();
+
+		$this->redirectMessage('msg_quote_added', [
+			$quote->getID()
+		], href('Quotes', 'Table'));
+	}
+
 }
 
