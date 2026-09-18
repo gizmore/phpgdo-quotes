921b2bdb475a021794386d57f1abe25f76993e77
gizmore <gizmore@wechall.net>
2026-09-18 04:07:20 +0200

    'more korean'

---
+++ lang/quotes_ko.php
@@ -0,0 +1,23 @@
+<?php
+namespace GDO\Quotes\lang;
+
+return [
+	'cfg_quote_sidebar' => '사이드바에 메소드를 추가하시겠습니까?',
+	'cfg_quote_likes' => '따옴표 같은 것을 허용하시겠습니까?',
+	'cfg_quote_guest_likes' => '손님이 좋아하도록 허용하시겠습니까?',
+	'cfg_quote_max_likes' => '사용자당 최대 좋아요 수 및 견적',
+	'cfg_quote_likes_minscore' => '좋아요를 누를 수 있는 최소 레벨',
+	'gdo_quote' => '견적',
+	'link_add_quote' => '견적 추가',
+	'link_quotes' => '%s 인용문',
+	'link_random_quote' => '무작위 견적',
+	'mt_quotes_table' => '인용문',
+	'md_quotes_table' => '모든 견적의 개요입니다.',
+	'list_quotes_table' => '%s 인용문을 찾았습니다.',
+	'quote_by_user' => '저자 / 너처',
+	'quote_by_whom' => '작성자',
+	'msg_quote_added' => '견적이 ID:%s(으)로 추가되었습니다.',
+	'mt_quotes_view' => '견적 보기',
+	'mt_quotes_like' => '인용문처럼',
+	'mt_quotes_dislike' => '인용문을 싫어함',
+];
